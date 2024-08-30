d04b36915093b5ab3b12bfe339dedbb62af360ef
Shreyash Galgale <shreyasgalgale019@gmail.com>
2024-08-30 22:57:01 +0530

    login fix

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,16 +1,19 @@
-const User = require('../models/user.models');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const User = require("../models/user.models");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 exports.registerUser = async (req, res) => {
     const { name, email, password } = req.body;
+
     try {
         const user = new User({ name, email, password });
         await user.save();
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "1d",
+        });
         res.status(201).json({ token });
     } catch (error) {
-        res.status(500).json({ error: 'Server error' });
+        res.status(500).json({ error });
     }
 };
 
@@ -18,12 +21,29 @@ exports.loginUser = async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await User.findOne({ email });
-        if (!user || !(await user.matchPassword(password))) {
-            return res.status(401).json({ error: 'Invalid credentials' });
+        console.log(user);
+
+        if (!user) {
+            return res.status(401).json({ error: "Invalid credentials" });
         }
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
-        res.json({ token });
+
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+            if (err) {
+                return res.status(500).json({ error: "Server error" });
+            }
+
+            if (isMatch) {
+                const token = jwt.sign(
+                    { id: user._id },
+                    process.env.JWT_SECRET,
+                    { expiresIn: "1d" }
+                );
+                return res.json({ status: "success", token });
+            } else {
+                return res.status(401).json({ error: "Invalid credentials" });
+            }
+        });
     } catch (error) {
-        res.status(500).json({ error: 'Server error' });
+        res.status(500).json({ error: "Server error" });
     }
 };
