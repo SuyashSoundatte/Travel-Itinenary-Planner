1d76bb44bf900b21a7f07fc5e919998f6f762839
Shreyash Galgale <shreyasgalgale019@gmail.com>
2024-08-30 20:58:36 +0530

    mail

--- backend/controllers/mail.js
+++ backend/controllers/mail.js
@@ -1,3 +1,40 @@
 //connect to mail server
 // send mail
 // https://www.mailjet.com/pricing/
+
+function sendMail(from, to, msg) {
+    const mailjet = require("node-mailjet").connect(
+        process.env.MJ_API_KEY,
+        process.env.MJ_SECRET_KEY
+    );
+    const request = mailjet.post("send", { version: "v3.1" }).request({
+        Messages: [
+            {
+                From: {
+                    Email: "shreyashgalgale000@gmail.com",
+                    Name: "Mailjet Pilot",
+                },
+                To: [
+                    {
+                        Email: "shreyasgalgale019@gmail.com",
+                        Name: "shreyash galgale",
+                    },
+                ],
+                Subject: "Your email flight plan!",
+                TextPart:
+                    "Dear passenger 1, welcome to Mailjet! May the delivery force be with you!",
+                HTMLPart:
+                    '<h3>Dear passenger 1, welcome to <a href="https://www.mailjet.com/">Mailjet</a>!</h3><br />May the delivery force be with you!',
+            },
+        ],
+    });
+    request
+        .then((result) => {
+            console.log(result.body);
+        })
+        .catch((err) => {
+            console.log(err.statusCode);
+        });
+}
+
+sendMail("from", "to", "msg");
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,2 +1,16 @@
 //checkAuth
 //verify JWT token
+const jwt = require("jsonwebtoken");
+function checkAuth(req, res, next) {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.redirect("");
+    }
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(404).send("user not signed in");
+        } else {
+            next();
+        }
+    });
+}
